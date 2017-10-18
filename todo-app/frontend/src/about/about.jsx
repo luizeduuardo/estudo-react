effde39c67dd160ff80e484358623064dd864846
Luiz Eduardo da Silva <luizsilva@multconnect.com.br>
2017-10-18 16:55:19 -0200

    First commit

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem Ipsum</p>
+        <h2>Nossa História</h2>
+        <p>Lorem Ipsum</p>
+        <h2>Imprensa</h2>
+        <p>Lorem Ipsum</p>
+        
+    </div>
+)
